25469d1cee7b6da529af56093009d0d7a7885e6e
saramccombs <theSaraMcCombs@gmail.com>
2019-10-04 12:17:13 -0400

    began book class

---
+++ lib/wotcompanion/book.rb
@@ -0,0 +1,28 @@
+class WOTcompanion::Book
+
+    def self.all
+        # I return instances of all books within the series
+        puts <<-DOC
+
+        BOOKS IN SERIES:
+
+        0. New Spring
+        1. The Eye of the World
+        2. The Great Hunt
+        3. The Dragon Reborn
+        4. The Shadow Rising
+        5. The Fires of Heaven
+        6. Lord of Chaos
+        7. A Crown of Swords
+        8. The Path of Daggers
+        9. Winter's Heart
+        10. Crossroads of Twilight
+        11. Knife of Dreams
+        12. The Gathering Storm
+        13. Towers of Midnight
+        14. A Memory of Light
+
+        DOC
+    end
+
+end
